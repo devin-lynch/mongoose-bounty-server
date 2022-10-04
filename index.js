99cd9f9b7a956a50acf694c88f4491ec2f562ce0
devin-lynch <devin.l.lynch@gmail.com>
2022-10-04 15:52:35 -0500

    Listen on port

---
+++ index.js
@@ -0,0 +1,25 @@
+// required packages
+const express = require('express')
+const cors = require('cors')
+
+// app configuration
+const app = express()
+const PORT = 8000
+// connect to database
+require('./models')
+
+// middlewares
+// allow cross origin resource sharing (cors)
+app.use(cors())
+// enable json request body parsing
+app.use(express.json())
+
+// routes/controllers
+app.get('./', (req, res) => {
+    res.json({ message: 'Welcome to the Bounty Hunter Json API' 🤠 })
+})
+
+// listen on a port!
+app.listen(PORT, () => {
+    console.log(`Tuned in to the smooth sounds of port ${PORT}`)
+})
