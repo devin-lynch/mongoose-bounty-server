2c5777536354083ce9980fc4ad5f35efdcf8b9e8
devin-lynch <devin.l.lynch@gmail.com>
2022-10-04 13:35:03 -0500

    Set bounty model

--- models/Bounty.js
+++ models/Bounty.js
@@ -1,2 +1,30 @@
 // require the mongoose package
 const mongoose = require('mongoose')
+
+// define the mongoose schema (key/value pairs of what our models should be)
+const BountySchema = new mongoose.Schema({
+    name: { 
+        type: String
+    }, 
+    wantedFor: { 
+        type: String
+    },
+    client: { 
+        type: String 
+    },
+    ship: { 
+        type: String 
+    },
+    reward: { 
+        type: Number 
+    },
+    captured: { 
+        type: Boolean 
+    },
+    lastSeen: { 
+        type: String 
+    },
+    
+}, {
+    timestamps: true
+})
